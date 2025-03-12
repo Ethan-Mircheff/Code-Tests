00e74bd0bdf5f90bb6b02ffe59e8ec00a8706485
Ethan Mircheff <163207625+Ethan-Mircheff@users.noreply.github.com>
2025-03-12 13:13:11 -0700

    Added 150ms transition to make overlay loading look smother

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $(".imgSmall").click(function () {
         $("#imgBig").attr("src", $(this).prop('src'));
-        $("#clickOverlay").show();
-        $("#clickOverlayContent").show();
+        $("#clickOverlay").show(150);
+        $("#clickOverlayContent").show(150);
     });
 
     $("#imgBig, #clickOverlay").click(function () {
         $("#imgBig").attr("src", "");
-        $("#clickOverlay").hide();
-        $("#clickOverlayContent").hide();
+        $("#clickOverlay").hide(150);
+        $("#clickOverlayContent").hide(150);
     });
 });
